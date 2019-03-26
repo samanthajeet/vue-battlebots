bbd3721eb9f4467bb48c9c6ba1a207874297b31f
Samantha Jeet <samanthajeet@gmail.com>
2019-03-26 14:56:16 -0600

    delete bot function

---
+++ src/components/Bot.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>{{botObj.botName}}</h2>
+    <p>Attack: {{botObj.attackVal}}</p>
+    <p>Health: {{botObj.healthVal}}</p>
+    <button> SELECT </button>
+    <button @click='retireBot(index)'> RETIRE</button>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['botObj', 'retireBot', 'index']
+  
+}
+</script>
+
--- src/components/BotList.vue
+++ src/components/BotList.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    <h1>Bot List</h1>
+    <bot v-for='(botObj, i) in this.allBots' :key='i' :botObj='botObj' :index='i' :retireBot='retireBot' />
   </div>
 </template>
 
 <script>
+
+import Bot from './Bot'
+
 export default {
+  props: ['allBots'],
+  data: function(){
+    return {
+
+    }
+  },
+
+  methods: {
+    retireBot: function(index){
+      console.log(index)
+      this.allBots.splice(index,1)
+
+    }
+  },
+  components: { Bot : Bot}
   
 }
 </script>
