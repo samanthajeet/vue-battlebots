0fbe7c701aae0c46b4d365c1a16479e69e27cc25
Samantha Jeet <samanthajeet@gmail.com>
2019-03-26 14:06:26 -0600

    create bot, toggle components

---
+++ src/components/BotList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Bot List</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
---
+++ src/components/CreateBot.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>create bot component</h1>
+    <div>
+      <p>Bot Name</p>
+      <input type="text" v-model='botName'>
+    </div>
+    <div>
+      <p>Attack Value</p>
+      <input type="text" v-model='attackVal'>
+    </div>
+    <div>
+      <p>Health Value</p>
+      <input type="text" v-model='healthVal'>
+    </div>
+    <button @click='createBot' >create bot</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ['allBots','addBot'],
+  data: function(){
+    return {
+      botName: '',
+      attackVal: '',
+      healthVal: ''
+    }
+  },
+  methods: {
+    createBot: function(){
+      const newBot = { botName: this.botName, attackVal: this.attackVal, healthVal: this.healthVal}
+      this.addBot(newBot)
+    }
+  }
+  
+}
+</script>
